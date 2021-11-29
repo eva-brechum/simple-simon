d68b6465d407c7f08b46eaa1bcb831d82701bf80
Eva Brechum <Brechum.Eva@gmail.com>
2021-11-28 19:30:55 -0600

    working on simple simon

---
+++ index.js
@@ -0,0 +1,7 @@
+var topLeft = document.querySelector(".top-left-circle");
+var topRight = document.querySelector(".top-right-circle");
+var bottomLeft = document.querySelector(".bottom-left-circle");
+var bottomRight = document.querySelector(".bottom-left-circle")
+
+var sequence = [topLeft, topRight, bottomLeft, bottomRight];
+
